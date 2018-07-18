7a0781d4dbcf5c475f1e37d3a576b9ed6396e1bc
1490237067@qq.com <1490237067@qq.com>
2018-07-18 20:59:36 +0800

    Android 加载时动画显示

---
+++ app/src/main/java/com/example/huangcl/loadinganimationdemo/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.huangcl.loadinganimationdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+
+import com.github.ybq.android.spinkit.SpinKitView;
+import com.github.ybq.android.spinkit.style.ChasingDots;
+import com.github.ybq.android.spinkit.style.CubeGrid;
+import com.github.ybq.android.spinkit.style.DoubleBounce;
+import com.github.ybq.android.spinkit.style.FoldingCube;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
